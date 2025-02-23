78d884cf3586cbbe3699d679617e6c145c8fb7cc
Douglas Storz <douglasstorz@gmail.com>
2025-02-23 04:47:28 -0500

    Extracted contact card to component

--- cat-rolodex/src/App.jsx
+++ cat-rolodex/src/App.jsx
@@ -6,31 +6,15 @@
  *     - Think ahead: what's the problem with doing it this way?
  */
 
+import Contact from './components/Contact'
+
 function App() {
     return (
         <div className="contacts">
-            <article className="contact-card">
-                <img 
-                    src="mr-whiskerson.png"
-                    alt="Photo of Mr. Whiskerson"
-                />
-                <h3>Mr. Whiskerson</h3>
-                <div className="info-group">
-                    <img 
-                        src="phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-1234</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>mr.whiskaz@catnap.meow</p>
-                </div>
-            </article>
-            
+            <Contact />
+            <Contact />
+            <Contact />
+            <Contact />
             <article className="contact-card">
                 <img 
                     src="fluffykins.png"
