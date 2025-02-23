297b2e602d19f34b78664682b0f43e9afb1fdd8f
Douglas Storz <douglasstorz@gmail.com>
2025-02-23 17:26:41 -0500

    Better CSS and aria-label for form input

--- chef-claude/components/SearchForm.jsx
+++ chef-claude/components/SearchForm.jsx
@@ -2,9 +2,14 @@ export default function SearchForm() {
   return (
     <main>
       <form className="ingredient-form">
-        <input type="text" name="ingredient" placeholder="e.g. oregano" />
+        <input
+          type="text"
+          name="ingredient"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+        />
         <button type="sumbit" name="add-ingredient">
-          + Add ingredient
+          Add ingredient
         </button>
       </form>
     </main>
