84636ac36a4dbb4f1030cc9a699c063fef0a093a
Douglas Storz <douglasstorz@gmail.com>
2025-02-23 17:05:18 -0500

    Header section

--- chef-claude/components/Header.jsx
+++ chef-claude/components/Header.jsx
@@ -1,7 +1,9 @@
+import chefClaudeIcon from "../images/chef-claude-icon.png";
+
 export default function Header() {
   return (
     <header>
-      <img src="./images/chef-claude-icon.png" alt="Chef Claude icon" />
+      <img src={chefClaudeIcon} alt="Chef Claude icon" />
       <span>Chef Claude</span>
     </header>
   );
