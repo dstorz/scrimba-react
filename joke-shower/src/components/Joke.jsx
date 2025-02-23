6f25704cfc7034c3c6f195adbbe45c41c5cdf612
Douglas Storz <douglasstorz@gmail.com>
2025-02-23 08:29:51 -0500

    Playing with how to show comments

--- joke-shower/src/components/Joke.jsx
+++ joke-shower/src/components/Joke.jsx
@@ -1,3 +1,23 @@
+//function makeComment(comment) {
+function Comment({ message }) {
+  // This is very similar to a component function, but will not work as
+  // a component because it accepts a string as its argument
+
+  /*
+  I wanted to do this with a function instead of an inline
+  anonymous function for clarity, but looking at how close it is to a component,
+  maybe there's a better pattern.
+
+  There's an argument to be made that the better patern is:
+
+  <ul>
+    {comments.map((comment) => <Comment msg={comment} />}
+  </ul>
+  */
+
+  return <li>{message}</li>;
+}
+
 export default function Joke({
   setup,
   punchline,
@@ -13,9 +33,9 @@ export default function Joke({
         👍{upvotes} 👎{downvotes}
       </div>
       <ul className="comment-list">
-        {comments.map((comment) => {
-          return <li>{comment}</li>;
-        })}
+        {comments.map((comment) => (
+          <Comment message={comment} />
+        ))}
       </ul>
     </div>
   );
