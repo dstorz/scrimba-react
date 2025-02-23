25a88771160c4faaee2986932b51c284b7a11012
Douglas Storz <douglasstorz@gmail.com>
2025-02-23 09:01:44 -0500

    Extracted joke data as "json"

---
+++ joke-shower/src/jokesData.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    setup: "What do you get if you cross an elephant and a rhino?",
+    punchline: "Elephino",
+    upvotes: 0,
+    downvotes: 0,
+    comments: {},
+  },
+  {
+    setup: "What's a pirate's favorite letter?",
+    punchline: "Arrr.",
+    upvotes: 10,
+    downvotes: 0,
+    comments: {},
+  },
+  {
+    setup: "How did the hacker escape from the police?",
+    punchline: "He just randomware.",
+    upvotes: 0,
+    downvotes: 0,
+    comments: {},
+  },
+  {
+    setup: "Why was 6 afraid of 7?",
+    punchline: "Because 7 8 9.",
+    upvotes: 4,
+    downvotes: 8,
+    comments: { comments },
+  },
+  {
+    setup: "Does Fungalore grant wishes?",
+    punchline: "No, Fungalore hears them only.",
+    upvotes: 0,
+    downvotes: 0,
+    comments: { comments },
+  },
+  {
+    punchline: "Feed fish man eat fish teach fish lifetime",
+    upvotes: 0,
+    downvotes: 0,
+    comments: { comments },
+  },
+];
