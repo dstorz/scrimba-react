e8b5189d5782b615ebd3755ae711c2d580b634c8
Douglas Storz <douglasstorz@gmail.com>
2025-02-24 21:07:00 -0500

    Conditional rendering of ingredient list and button

--- chef-claude/components/Main.jsx
+++ chef-claude/components/Main.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 export default function Main() {
-  const [ingredients, setIngredients] = useState([
-    "Chicken",
-    "Oregano",
-    "Tomatoes",
-  ]);
+  // const [ingredients, setIngredients] = useState([
+  //   "Chicken",
+  //   "Oregano",
+  //   "Tomatoes",
+  // ]);
+  const [ingredients, setIngredients] = useState([]);
 
   function handleSubmit(event) {
     /* Imperative way of handling form submission. Meant to be given to the onSubmit attribute
@@ -51,7 +52,23 @@ export default function Main() {
           Add ingredient
         </button>
       </form>
-      <ul>{ingredientElements}</ul>
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientElements}
+          </ul>
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   );
 }
