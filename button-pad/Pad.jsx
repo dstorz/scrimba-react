48218ebf39057791b86b3477f80f57710d593c65
Douglas Storz <douglasstorz@gmail.com>
2025-02-26 02:36:56 -0500

    Pads update their own on status on click

--- button-pad/Pad.jsx
+++ button-pad/Pad.jsx
@@ -1,6 +1,15 @@
-export default function Pad({ color, on }) {
+import { useState } from "react";
+
+export default function Pad({ color, on: initOn }) {
+  const [on, setOn] = useState(initOn);
+
+  function toggle() {
+    setOn((prev) => !prev);
+  }
+
   return (
     <button
+      onClick={toggle}
       style={{ backgroundColor: color }}
       className={on ? "on" : undefined} // React recommends using undefined instead of null, but both work
     ></button>
