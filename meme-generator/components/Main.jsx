6852f9e811dbf8dc7a51946f25a402fa30b07b1f
Douglas Storz <douglasstorz@gmail.com>
2025-02-27 03:47:12 -0500

    Fetches memes and button randomly selects one

--- meme-generator/components/Main.jsx
+++ meme-generator/components/Main.jsx
@@ -1,23 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const IMGFLIP_API_URL = "https://api.imgflip.com/get_memes";
 
 export default function Main() {
-  /**
-   * Challenge: move the hardcoded meme info into React
-   * state. Use an object with `topText`, `bottomText`,
-   * and `image` properties, and set the initial values to
-   * the ones hardcoded below.
-   */
   const [meme, setMeme] = useState({
     topText: "One does not simply",
     bottomText: "Walk into Mordor",
     image: "http://i.imgflip.com/1bij.jpg",
   });
+  /* Structure of one meme in memeSet array
+    {
+      "id": "101470",
+      "name": "Ancient Aliens",
+      "url": "https://i.imgflip.com/26am.jpg",
+      "width": 500,
+      "height": 437,
+      "box_count": 2
+    } 
+  */
+  const [memeSet, setMemeSet] = useState([]);
+
+  useEffect(() => {
+    fetch(IMGFLIP_API_URL)
+      .then((res) => res.json())
+      .then((data) => setMemeSet(data.data.memes));
+  }, []);
 
   function handleChange(event) {
     const { value, name } = event.currentTarget;
     setMeme((prev) => {
       return { ...prev, [name]: value };
     });
+  }
+
+  function getMeme() {
+    const randomIdx = Math.floor(Math.random() * (memeSet.length - 1));
+    setMeme((prev) => ({
+      ...prev,
+      image: memeSet[randomIdx].url,
+    }));
   }
 
   return (
@@ -44,7 +65,7 @@ export default function Main() {
             value={meme.bottomText}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getMeme}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={meme.image} />
