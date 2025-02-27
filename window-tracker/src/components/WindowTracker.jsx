095e4bcbe7d136eb6e7bdb2661b398d50a2eb1ff
Douglas Storz <douglasstorz@gmail.com>
2025-02-27 04:52:30 -0500

    Using state and an effect with a cleanup function to track window width

--- window-tracker/src/components/WindowTracker.jsx
+++ window-tracker/src/components/WindowTracker.jsx
@@ -1,3 +1,19 @@
+import { useState, useEffect } from "react";
+
 export default function WindowTracker() {
-  return <h1>Window Width: {window.innerWidth}px</h1>;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function watchWindowWidth() {
+      console.log("Resized!");
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", watchWindowWidth);
+
+    return () => {
+      window.removeEventListener("resize", watchWindowWidth);
+    };
+  }, []);
+
+  return <h1>Window Width: {width}px</h1>;
 }
