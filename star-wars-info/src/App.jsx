9cf605166b4a53234458f2d35b408bcb39d0ce8c
Douglas Storz <douglasstorz@gmail.com>
2025-02-27 02:58:46 -0500

    useEffect with a dependency

--- star-wars-info/src/App.jsx
+++ star-wars-info/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [starWarsInfo, setStarWarsInfo] = useState({});
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/1")
+      .then((res) => res.json())
+      .then((data) => setStarWarsInfo(data));
+  }, [count]);
+
+  function add() {
+    setCount((prev) => prev + 1);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        The count is {count}
+        <br />
+        <button onClick={add}>Add</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <pre>{JSON.stringify(starWarsInfo, null, 2)}</pre>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
