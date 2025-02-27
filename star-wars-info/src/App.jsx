3faa3244769bd3a447ae0fbfafc1edb094b808fd
Douglas Storz <douglasstorz@gmail.com>
2025-02-27 03:15:22 -0500

    Extra credit: Depending on count and using it to determine the person ID to fetch

--- star-wars-info/src/App.jsx
+++ star-wars-info/src/App.jsx
@@ -5,21 +5,22 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/people/1")
+    fetch(`https://swapi.dev/api/people/${count + 1}`)
       .then((res) => res.json())
       .then((data) => setStarWarsInfo(data));
-  }, []);
-
-  function add() {
-    setCount((prev) => prev + 1);
-  }
+  }, [count]);
 
   return (
     <>
       <div>
-        The count is {count}
+        Person ID: {count + 1}
         <br />
-        <button onClick={add}>Add</button>
+        <button
+          onClick={() => setCount((prev) => (prev - 1 > 0 ? prev - 1 : 0))}
+        >
+          Previous
+        </button>
+        <button onClick={() => setCount((prev) => prev + 1)}>Next</button>
       </div>
       <div>
         <pre>{JSON.stringify(starWarsInfo, null, 2)}</pre>
