37459a38598212b159f1acb4f9de523c08744542
Douglas Storz <douglasstorz@gmail.com>
2025-02-27 01:21:09 -0500

    Better style for recipe section

--- chef-claude/components/ClaudeRecipe.jsx
+++ chef-claude/components/ClaudeRecipe.jsx
@@ -2,7 +2,8 @@ import ReactMarkdown from "react-markdown";
 
 export default function ClaudeRecipe({ recipe }) {
   return (
-    <section>
+    <section className="suggested-recipe-container" aria-live="polite">
+      <h2>Chef Claude recommends:</h2>
       <ReactMarkdown>{recipe}</ReactMarkdown>
     </section>
   );
